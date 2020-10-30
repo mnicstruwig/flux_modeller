049d768264b1613e798a818a8a10b3e6376a8832
Michael Struwig <michael@praelexis.com>
2020-10-30 12:19:29 +0200

    Rename project

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="flux modeller",
+    version="0.0.1",
+    author="Michael Struwig",
+    author_email="mnicstruwig@gmail.com",
+    description="Model flux curves",
+    packages=['flux_modeller'],
+    python_requires='>=3.7',
+)
